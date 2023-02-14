7490ec135b21fc43377b904dcf45f58c3644506a
Sreeram A <sriramlibra0@gmail.com>
2023-02-14 20:26:37 +0530

    deployment connect to mongo db error

--- model/file.js
+++ model/file.js
@@ -1,10 +1,5 @@
 const mongoose=require('mongoose');
 
-const multer=require('multer');
-
-// const path=require('path');
-
-// const FILE_PATH=path.join('/uploads/users/files');
 
 const FileSchema= new mongoose.Schema({
     file:{
