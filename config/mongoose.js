f798f59514e8a7160dad3a12c37cb2d43030e19f
Sreeram A <sriramlibra0@gmail.com>
2023-02-23 10:41:31 +0530

    error

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,10 @@ mongoose.set('strictQuery', true);
 
 console.log(process.env.DB_CONNECT);
 
-mongoose.connect(process.env.DB_CONNECT).then(()=>{
+// mongoose.connect(process.env.DB_CONNECT).then(()=>{
+mongoose.connect("mongodb+srv://sriram_arumugham:%21ifeisBeauti5ul@cluster0.6z5scin.mongodb.net/test?retryWrites=true&w=majority").then(()=>{
+
+    
     console.log("Atlas DB connected")
 }).catch(err=>{
     console.log("mongod db failed to connect", err );
